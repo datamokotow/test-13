9c26ba4ad479537fb5e5f09eb689f389471eddc3
datamokotow <a_rutvik@yahoo.com>
2023-09-17 22:47:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,129 +4,124 @@ from langchain.document_loaders import PyPDFLoader, Docx2txtLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 import pinecone
-import pinecone
 from langchain.vectorstores import Pinecone
-from langchain.embeddings.openai import OpenAIEmbeddings
 import tempfile
 import tiktoken
 
+# Initialize environment variables for API keys
 os.environ["OPENAI_API_KEY"] = st.secrets['OPENAI_API_KEY']
 pinecone.init(api_key=st.secrets['PINECONE_API_KEY'], environment=st.secrets['PINECONE_ENVIRONMENT'])
-pinecone.init(      
-	api_key=st.secrets['PINECONE_API_KEY'],      
-	environment=st.secrets['PINECONE_ENVIRONMENT']      
-)      
+
+# Initialize Pinecone index
 index = pinecone.Index('test-01')
 
+# Document Processing Functions
 
-# Funciones de procesamiento de documentos
-def cargar_documento(archivo):
+# Load Document
+def load_document(file):
     if not os.path.exists("docs"):
         os.makedirs("docs")
 
-    ruta_archivo = os.path.join("docs", archivo.name)
-    with open(ruta_archivo, 'wb') as f:
-        f.write(archivo.read())
+    file_path = os.path.join("docs", file.name)
+    with open(file_path, 'wb') as f:
+        f.write(file.read())
 
-    nombre, extension = os.path.splitext(archivo.name)
+    name, extension = os.path.splitext(file.name)
     if extension == '.pdf':
-        st.write(f'Cargando {archivo.name}...')
-        loader = PyPDFLoader(ruta_archivo)
+        st.write(f'Loading {file.name}...')
+        loader = PyPDFLoader(file_path)
     elif extension == '.docx':
-        st.write(f'Cargando {archivo.name}...')
-        loader = Docx2txtLoader(ruta_archivo)
+        st.write(f'Loading {file.name}...')
+        loader = Docx2txtLoader(file_path)
     else:
-        st.write('El formato del documento no está soportado!')
+        st.write('Document format not supported!')
         return None
 
     data = loader.load()
     return data
 
-def fragmentar(data, chunk_size=3530):
+# Split Text
+def split_text(data, chunk_size=3530):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=0)
-    fragmentos = text_splitter.split_documents(data)
-    return fragmentos
+    chunks = text_splitter.split_documents(data)
+    return chunks
 
-def costo_embedding(texts):
+# Calculate Embedding Cost
+def embedding_cost(texts):
     enc = tiktoken.encoding_for_model('text-embedding-ada-002')
     total_tokens = sum([len(enc.encode(page.page_content)) for page in texts])
     st.write(f'Total Tokens: {total_tokens}')
     st.write(f'Embedding Cost in USD: {total_tokens / 1000 * 0.0001:.5f}')
 
-#@st.cache(allow_output_mutation=True)
+# Cache Pinecone Index Deletion
 @st.cache_data()
-def borrar_indices(index_name='alter'):
-    if index_name == 'alter':
+def delete_indexes(index_name=index):
+    if index_name == index:
         indexes = pinecone.list_indexes()
-        st.write('Borrando todos los índices ... ')
+        st.write('Deleting all indexes ... ')
         for index in indexes:
             pinecone.delete_index(index)
-        st.write('Listo!')
+        st.write('Done!')
     else:
-        st.write(f'Borrando el índice: {index_name} ...', end='')
+        st.write(f'Deleting the index: {index_name} ...', end='')
         pinecone.delete_index(index_name)
-        st.write('Listo')
+        st.write('Done')
 
+# Create Vectors
 @st.cache(allow_output_mutation=True)
-def creando_vectores(index_name, fragmentos):
+def create_vectors(index_name, chunks):
     embeddings = OpenAIEmbeddings()
 
     if index_name in pinecone.list_indexes():
-        st.write(f'El índice {index_name} ya existe. Cargando los embeddings ... ')
-        vectores = Pinecone.from_existing_index(index_name, embeddings)
+        st.write(f'The index {index_name} already exists. Loading embeddings ... ')
+        vectors = Pinecone.from_existing_index(index_name, embeddings)
     else:
-        st.write(f'Creando el índice {index_name} y los embeddings ...')
+        st.write(f'Creating the index {index_name} and embeddings ...')
         pinecone.create_index(index_name, dimension=1536, metric='cosine')
-        vectores = Pinecone.from_documents(fragmentos, embeddings, index_name=index_name)
+        vectors = Pinecone.from_documents(chunks, embeddings, index_name=index_name)
 
     st.write('Ok')
-    return vectores
-def consulta_con_memoria(vectores, pregunta, memoria=[]):
+    return vectors
+
+# Memory-based Query
+def query_with_memory(vectors, question, memory=[]):
     from langchain.chains import ConversationalRetrievalChain
     from langchain.chat_models import ChatOpenAI
     llm = ChatOpenAI(temperature=0, model_name="gpt-4")
-    retriever = vectores.as_retriever(search_type='similarity', search_kwargs={'k': 5})
+    retriever = vectors.as_retriever(search_type='similarity', search_kwargs={'k': 5})
     crc = ConversationalRetrievalChain.from_llm(llm, retriever)
-    respuesta = crc({'question': pregunta, 'chat_history': memoria})
-    memoria.append((pregunta, respuesta['answer']))
+    answer = crc({'question': question, 'chat_history': memory})
+    memory.append((question, answer['answer']))
 
-    return respuesta, memoria
+    return answer, memory
 
-
+# Main Function
 def main():
-    # Montar Google Drive
-    # Cargar archivo desde el input
-    st.title("Chatbot de Preguntas y Respuestas")
-    #CSS al input 
+    st.title("Question and Answer Chatbot")
+    file = st.file_uploader("Select a PDF or DOCX file", type=["pdf", "docx"])
+    if file:
+        content = load_document(file)
+        if content:
+            chunks = split_text(content)
+            st.write(f'The number of chunks is: {len(chunks)} chunks')
 
-    archivo = st.file_uploader("Selecciona un archivo PDF o DOCX", type=["pdf", "docx"])
-    if archivo:
-        contenido = cargar_documento(archivo)
-        if contenido:
-            fragmentos = fragmentar(contenido)
-            st.write(f"El Número de fragmentos es de: {len(fragmentos)} fragmentos")
+            # Calculate embedding cost
+            embedding_cost(chunks)
 
-            # Calcular costo de embedding
-            costo_embedding(fragmentos)
-
-            # Crear índice y vectores
-            borrar_indices("alter")
+            # Create index and vectors
+            delete_indexes("alter")
             index_name = 'alter'
-            vectores = creando_vectores(index_name, fragmentos)
+            vectors = create_vectors(index_name, chunks)
 
-            # Consultas
-            preguntas(vectores)
-            # pregunta = st.text_input("Escribe tu pregunta:")
-            # if pregunta:
-            #     respuesta, memoria = consulta_con_memoria(vectores, pregunta, memoria=[])
-            #     st.write("Respuesta:", respuesta['answer'])
+            # Queries
+            questions(vectors)
 
-def preguntas(vectores):
-    st.subheader("Consulta de Preguntas")
-    pregunta = st.text_input("Escribe tu pregunta:")
-    if pregunta:
-        respuesta, memoria = consulta_con_memoria(vectores, pregunta, memoria=[])
-        st.write("Respuesta:", respuesta['answer'])
+def questions(vectors):
+    st.subheader("Question Query")
+    question = st.text_input("Write your question:")
+    if question:
+        answer, memory = query_with_memory(vectors, question, memory=[])
+        st.write("Answer:", answer['answer'])
 
 if __name__ == "__main__":
     main()
