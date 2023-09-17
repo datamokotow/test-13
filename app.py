c58ed652934300319ce84fe7c504ae6e766ba6b6
datamokotow <a_rutvik@yahoo.com>
2023-09-17 22:15:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ import tiktoken
 #os.environ["OPENAI_API_KEY"] = "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"
 #pinecone.init(api_key='500b9641-7f7d-463c-8970-6668996aa53a', environment='asia-southeast1-gcp-free')
 
-os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
-pinecone.init(api_key="PINECONE_API_KEY", environment="PINECONE_ENVIRONMENT")
+os.environ["OPENAI_API_KEY"] = st.secrets(OPENAI_API_KEY)
+pinecone.init(api_key=st.secrets(PINECONE_API_KEY), environment=st.secrets(PINECONE_ENVIRONMENT)
 
 # Funciones de procesamiento de documentos
 def cargar_documento(archivo):
